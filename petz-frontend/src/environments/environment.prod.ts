1ab6f402f12ac3ce1e7449eb8f3cf1fe322bb9d7
Bhargava <2480002@cognizant.com>
2026-05-06 13:47:29 +0530

    railway configuration set up

--- petz-frontend/src/environments/environment.prod.ts
+++ petz-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://YOUR_RAILWAY_URL/api/v1',
-  wsUrl: 'https://YOUR_RAILWAY_URL/api/v1/ws'
+  apiUrl: 'https://petz-production.up.railway.app/api/v1',
+  wsUrl: 'https://petz-production.up.railway.app/api/v1/ws'
 };
